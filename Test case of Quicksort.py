68bec2b7756593b0e793bdf2e5d0195b68071cdd
Ivanycc <ivan1226yu@gmail.com>
2026-04-16 23:28:48 +0800

    Delete Test case of Quicksort.py

--- Test case of Quicksort.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    print("\n=== QUICKSORT DEMO ===")
-    data = [10, 7, 8, 9, 1, 5]
-    print("Original:", data)
-    sorted_data = quicksort(data)
-    print("Sorted:", sorted_data)
